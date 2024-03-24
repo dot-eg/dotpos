76d417fe57a01b06e866bc455d255bda60dd4cfe
Omar Elfiki <oe2300062@tkh.edu.eg>
2024-03-24 10:30:56 +0200

    adding new UI

--- lib/main.dart
+++ lib/main.dart
@@ -127,48 +127,408 @@ class _MyHomePageState extends State<MyHomePage> {
 class ProductHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
-
-    IconData icon;
-    if (appState.favorites.contains(pair)) {
-      icon = Icons.favorite;
-    } else {
-      icon = Icons.favorite_border;
-    }
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Products', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold,),
-          ),
-        automaticallyImplyLeading: false,
+    body: Container(
+    width: 1366,
+    height: 1024,
+    clipBehavior: Clip.antiAlias,
+    decoration: BoxDecoration(color: Colors.white),
+    child: Stack(
+        children: [
+            Positioned(
+                left: 936,
+                top: 0,
+                child: Container(
+                    width: 430,
+                    height: 932,
+                    decoration: BoxDecoration(color: Color(0xFFA5A5A5)),
+                ),
+            ),
+            Positioned(
+                left: 936,
+                top: 0,
+                child: Container(
+                    width: 430,
+                    height: 81,
+                    decoration: BoxDecoration(color: Colors.black),
+                ),
+            ),
+            Positioned(
+                left: 61,
+                top: 71,
+                child: Container(
+                    width: 802,
+                    height: 826,
+                    child: Stack(
+                        children: [
+                            Positioned(
+                                left: 0,
+                                top: 0,
+                                child: Container(
+                                    width: 150,
+                                    height: 150,
+                                    decoration: BoxDecoration(color: Color(0xFFD9D9D9)),
+                                ),
+                            ),
+                            Positioned(
+                                left: 163,
+                                top: 0,
+                                child: Container(
+                                    width: 150,
+                                    height: 150,
+                                    decoration: BoxDecoration(color: Color(0xFFD9D9D9)),
+                                ),
+                            ),
+                            Positioned(
+                                left: 652,
+                                top: 0,
+                                child: Container(
+                                    width: 150,
+                                    height: 150,
+                                    decoration: BoxDecoration(color: Color(0xFFD9D9D9)),
+                                ),
+                            ),
+                            Positioned(
+                                left: 326,
+                                top: 0,
+                                child: Container(
+                                    width: 150,
+                                    height: 150,
+                                    decoration: BoxDecoration(color: Color(0xFFD9D9D9)),
+                                ),
+                            ),
+                            Positioned(
+                                left: 489,
+                                top: 0,
+                                child: Container(
+                                    width: 150,
+                                    height: 150,
+                                    decoration: BoxDecoration(color: Color(0xFFD9D9D9)),
+                                ),
+                            ),
+                            Positioned(
+                                left: 0,
+                                top: 169,
+                                child: Container(
+                                    width: 150,
+                                    height: 150,
+                                    decoration: BoxDecoration(color: Color(0xFFD9D9D9)),
+                                ),
+                            ),
+                            Positioned(
+                                left: 163,
+                                top: 169,
+                                child: Container(
+                                    width: 150,
+                                    height: 150,
+                                    decoration: BoxDecoration(color: Color(0xFFD9D9D9)),
+                                ),
+                            ),
+                            Positioned(
+                                left: 652,
+                                top: 169,
+                                child: Container(
+                                    width: 150,
+                                    height: 150,
+                                    decoration: BoxDecoration(color: Color(0xFFD9D9D9)),
+                                ),
+                            ),
+                            Positioned(
+                                left: 326,
+                                top: 169,
+                                child: Container(
+                                    width: 150,
+                                    height: 150,
+                                    decoration: BoxDecoration(color: Color(0xFFD9D9D9)),
+                                ),
+                            ),
+                            Positioned(
+                                left: 489,
+                                top: 169,
+                                child: Container(
+                                    width: 150,
+                                    height: 150,
+                                    decoration: BoxDecoration(color: Color(0xFFD9D9D9)),
+                                ),
+                            ),
+                            Positioned(
+                                left: 0,
+                                top: 507,
+                                child: Container(
+                                    width: 150,
+                                    height: 150,
+                                    decoration: BoxDecoration(color: Color(0xFFD9D9D9)),
+                                ),
+                            ),
+                            Positioned(
+                                left: 163,
+                                top: 507,
+                                child: Container(
+                                    width: 150,
+                                    height: 150,
+                                    decoration: BoxDecoration(color: Color(0xFFD9D9D9)),
+                                ),
+                            ),
+                            Positioned(
+                                left: 652,
+                                top: 507,
+                                child: Container(
+                                    width: 150,
+                                    height: 150,
+                                    decoration: BoxDecoration(color: Color(0xFFD9D9D9)),
+                                ),
+                            ),
+                            Positioned(
+                                left: 326,
+                                top: 507,
+                                child: Container(
+                                    width: 150,
+                                    height: 150,
+                                    decoration: BoxDecoration(color: Color(0xFFD9D9D9)),
+                                ),
+                            ),
+                            Positioned(
+                                left: 489,
+                                top: 507,
+                                child: Container(
+                                    width: 150,
+                                    height: 150,
+                                    decoration: BoxDecoration(color: Color(0xFFD9D9D9)),
+                                ),
+                            ),
+                            Positioned(
+                                left: 0,
+                                top: 676,
+                                child: Container(
+                                    width: 150,
+                                    height: 150,
+                                    decoration: BoxDecoration(color: Color(0xFFD9D9D9)),
+                                ),
+                            ),
+                            Positioned(
+                                left: 163,
+                                top: 676,
+                                child: Container(
+                                    width: 150,
+                                    height: 150,
+                                    decoration: BoxDecoration(color: Color(0xFFD9D9D9)),
+                                ),
+                            ),
+                            Positioned(
+                                left: 652,
+                                top: 676,
+                                child: Container(
+                                    width: 150,
+                                    height: 150,
+                                    decoration: BoxDecoration(color: Color(0xFFD9D9D9)),
+                                ),
+                            ),
+                            Positioned(
+                                left: 326,
+                                top: 676,
+                                child: Container(
+                                    width: 150,
+                                    height: 150,
+                                    decoration: BoxDecoration(color: Color(0xFFD9D9D9)),
+                                ),
+                            ),
+                            Positioned(
+                                left: 489,
+                                top: 676,
+                                child: Container(
+                                    width: 150,
+                                    height: 150,
+                                    decoration: BoxDecoration(color: Color(0xFFD9D9D9)),
+                                ),
+                            ),
+                            Positioned(
+                                left: 0,
+                                top: 338,
+                                child: Container(
+                                    width: 150,
+                                    height: 150,
+                                    decoration: BoxDecoration(color: Color(0xFFD9D9D9)),
+                                ),
+                            ),
+                            Positioned(
+                                left: 163,
+                                top: 338,
+                                child: Container(
+                                    width: 150,
+                                    height: 150,
+                                    decoration: BoxDecoration(color: Color(0xFFD9D9D9)),
+                                ),
+                            ),
+                            Positioned(
+                                left: 652,
+                                top: 338,
+                                child: Container(
+                                    width: 150,
+                                    height: 150,
+                                    decoration: BoxDecoration(color: Color(0xFFD9D9D9)),
+                                ),
+                            ),
+                            Positioned(
+                                left: 326,
+                                top: 338,
+                                child: Container(
+                                    width: 150,
+                                    height: 150,
+                                    decoration: BoxDecoration(color: Color(0xFFD9D9D9)),
+                                ),
+                            ),
+                            Positioned(
+                                left: 489,
+                                top: 338,
+                                child: Container(
+                                    width: 150,
+                                    height: 150,
+                                    decoration: BoxDecoration(color: Color(0xFFD9D9D9)),
+                                ),
+                            ),
+                        ],
+                    ),
+                ),
+            ),
+            Positioned(
+                left: 53,
+                top: 19,
+                child: SizedBox(
+                    width: 195,
+                    height: 42,
+                    child: Text(
+                        'Products',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 30,
+                            fontFamily: 'Content',
+                            fontWeight: FontWeight.w700,
+                            height: 0,
+                        ),
+                    ),
+                ),
+            ),
+            Positioned(
+                left: 974,
+                top: 840,
+                child: Container(
+                    width: 353,
+                    height: 57,
+                    child: Stack(
+                        children: [
+                            Positioned(
+                                left: 0,
+                                top: 0,
+                                child: Container(
+                                    width: 353,
+                                    height: 57,
+                                    child: Stack(
+                                        children: [
+                                            Positioned(
+                                                left: 37.50,
+                                                top: 12,
+                                                child: SizedBox(
+                                                    width: 277,
+                                                    height: 33,
+                                                    child: Text(
+                                                        'Payment',
+                                                        style: TextStyle(
+                                                            color: Colors.white,
+                                                            fontSize: 20,
+                                                            fontFamily: 'Content',
+                                                            fontWeight: FontWeight.w700,
+                                                            height: 0,
+                                                        ),
+                                                    ),
+                                                ),
+                                            ),
+                                        ],
+                                    ),
+                                ),
+                            ),
+                        ],
+                    ),
+                ),
+            ),
+            Positioned(
+                left: 1053,
+                top: 19,
+                child: SizedBox(
+                    width: 195,
+                    height: 42,
+                    child: Text(
+                        'Cart',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontFamily: 'Content',
+                            fontWeight: FontWeight.w700,
+                            height: 0,
+                        ),
+                    ),
+                ),
+            ),
+            Positioned(
+                left: 0,
+                top: 931.51,
+                child: Container(
+                    width: 1366,
+                    height: 92.49,
+                    child: Stack(
+                        children: [
+                            Positioned(
+                                left: 0,
+                                top: 0,
+                                child: Container(
+                                    width: 1366,
+                                    height: 92.49,
+                                    decoration: BoxDecoration(color: Colors.black),
+                                ),
+                            ),
+                            Positioned(
+                                left: 103.52,
+                                top: 23.98,
+                                child: Container(
+                                    width: 42.69,
+                                    height: 45.67,
+                                    decoration: ShapeDecoration(
+                                        color: Colors.white,
+                                        shape: OvalBorder(),
+                                    ),
+                                ),
+                            ),
+                            Positioned(
+                                left: 39.49,
+                                top: 23.98,
+                                child: Container(
+                                    width: 42.69,
+                                    height: 45.67,
+                                    decoration: ShapeDecoration(
+                                        color: Colors.white,
+                                        shape: OvalBorder(),
+                                    ),
+                                ),
+                            ),
+                            Positioned(
+                                left: 167.55,
+                                top: 23.98,
+                                child: Container(
+                                    width: 42.69,
+                                    height: 45.67,
+                                    decoration: ShapeDecoration(
+                                        color: Colors.white,
+                                        shape: OvalBorder(),
+                                    ),
+                                ),
+                            ),
+                        ],
+                    ),
+                ),
+            ),
+        ],
     ),
-
-    body: GridView.count(
-  crossAxisCount: 6,// number of columns
-  childAspectRatio: 2,
-  mainAxisSpacing: 20, 
-  children: List.generate(54, (index) { // replace 10 with the number of items you want
-    return Center(
-      child: Card(
-        child: Padding(
-          padding: const EdgeInsets.all(40),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'Product $index',
-                style: Theme.of(context).textTheme.headlineSmall,
-              ),
-              // Add more widgets here for each product
-            ],
-          ),
-        ),
-      ),
-    );
-  }),
-),
+)
     );
   }
 }
