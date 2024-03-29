4ec5a521d078fd02ab7e529b02a18444db55e215
Omar Elfiki <oe2300062@tkh.edu.eg>
2024-03-29 21:21:48 +0200

    adding new login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:english_words/english_words.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
-);
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+ runApp(MyApp());
+ await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
 }
 
 
@@ -52,8 +55,6 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
-// ...
-
 class MyHomePage extends StatefulWidget {
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -80,7 +81,7 @@ class _MyHomePageState extends State<MyHomePage> {
         page = Placeholder();
         break;
       case 4:
-        page = Placeholder();
+        page = LoginScreenPage();
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
@@ -92,7 +93,7 @@ class _MyHomePageState extends State<MyHomePage> {
             color: Theme.of(context).colorScheme.primaryContainer,
             child: page,
           ),
-          bottomNavigationBar: BottomNavigationBar(
+          bottomNavigationBar: selectedIndex == 4 ? null : BottomNavigationBar(
             items: const <BottomNavigationBarItem>[
               BottomNavigationBarItem(
                 icon: Icon(Icons.home),
@@ -130,6 +131,241 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+class LoginScreenPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: null,
+      body: Container(
+    width: 1528,
+    height: 1000,
+    clipBehavior: Clip.antiAlias,
+    decoration: BoxDecoration(color: Colors.white),
+    child: Stack(
+        children: [
+            Positioned(
+                left: 968,
+                top: 105,
+                child: SizedBox(
+                    width: 290,
+                    height: 78,
+                    child: Text(
+                        'Log In',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 30,
+                            fontFamily: 'Hind Kochi',
+                            fontWeight: FontWeight.w500,
+                            height: 0,
+                        ),
+                    ),
+                ),
+            ),
+            Positioned(
+                left: 0,
+                top: 0,
+                child: Container(
+                    width: 894,
+                    height: 1000,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: NetworkImage("https://images.unsplash.com/photo-1579546929518-9e396f3cc809?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+                            fit: BoxFit.fill,
+                        ),
+                    ),
+                ),
+            ),
+            Positioned(
+                left: 48,
+                top: 44,
+                child: SizedBox(
+                    width: 131,
+                    height: 74,
+                    child: Text(
+                        'dot.',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 70,
+                            fontFamily: 'Kollektif',
+                            fontWeight: FontWeight.w700,
+                            height: 0,
+                            letterSpacing: -5,
+                        ),
+                    ),
+                ),
+            ),
+            Positioned(
+                left: 968,
+                top: 193,
+                child: SizedBox(
+                    width: 486,
+                    height: 190,
+                    child: Stack(
+                        children: [
+                            Positioned(
+                                left: 0,
+                                top: 24,
+                                child: Container(
+                                    width: 486,
+                                    height: 56,
+                                    decoration: ShapeDecoration(
+                                        color: Colors.white,
+                                        shape: RoundedRectangleBorder(
+                                            side: BorderSide(
+                                                width: 1,
+                                                strokeAlign: BorderSide.strokeAlignOutside,
+                                                color: Color(0xFFC3BBBB),
+                                            ),
+                                            borderRadius: BorderRadius.circular(20),
+                                        ),
+                                    ),
+                                ),
+                            ),
+                            Positioned(
+                                left: 0,
+                                top: 134,
+                                child: Container(
+                                    width: 486,
+                                    height: 56,
+                                    decoration: ShapeDecoration(
+                                        color: Colors.white,
+                                        shape: RoundedRectangleBorder(
+                                            side: BorderSide(
+                                                width: 1,
+                                                strokeAlign: BorderSide.strokeAlignOutside,
+                                                color: Color(0xFFC4BBBB),
+                                            ),
+                                            borderRadius: BorderRadius.circular(20),
+                                        ),
+                                    ),
+                                ),
+                            ),
+                            Positioned(
+                                left: 4,
+                                top: 0,
+                                child: SizedBox(
+                                    width: 162,
+                                    height: 24,
+                                    child: Text(
+                                        'Username',
+                                        style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 20,
+                                            fontFamily: 'Hind Kochi',
+                                            fontWeight: FontWeight.w500,
+                                            height: 0,
+                                        ),
+                                    ),
+                                ),
+                            ),
+                            Positioned(
+                                left: 4,
+                                top: 110,
+                                child: SizedBox(
+                                    width: 162,
+                                    height: 24,
+                                    child: Text(
+                                        'Password',
+                                        style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 20,
+                                            fontFamily: 'Hind Kochi',
+                                            fontWeight: FontWeight.w500,
+                                            height: 0,
+                                        ),
+                                    ),
+                                ),
+                            ),
+                        ],
+                    ),
+                ),
+            ),
+            Positioned(
+                left: 968,
+                top: 414,
+                child: Container(
+                    width: 486,
+                    height: 65,
+                    decoration: ShapeDecoration(
+                        color: Colors.black,
+                        shape: RoundedRectangleBorder(
+                            side: BorderSide(width: 1),
+                            borderRadius: BorderRadius.circular(20),
+                        ),
+                        shadows: [
+                            BoxShadow(
+                                color: Color(0x3F000000),
+                                blurRadius: 4,
+                                offset: Offset(0, 2),
+                                spreadRadius: 1,
+                            )
+                        ],
+                    ),
+                ),
+            ),
+            Positioned(
+                left: 1169,
+                top: 437,
+                child: SizedBox(
+                    width: 84,
+                    height: 30,
+                    child: Text(
+                        'Log in',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                            fontFamily: 'Hind Kochi',
+                            fontWeight: FontWeight.w500,
+                            height: 0,
+                        ),
+                    ),
+                ),
+            ),
+            Positioned(
+                left: 1004,
+                top: 500,
+                child: SizedBox(
+                    width: 413,
+                    height: 23,
+                    child: Text(
+                        'Looking for the admin console? click here',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15,
+                            fontFamily: 'Hind Kochi',
+                            fontWeight: FontWeight.w500,
+                            height: 0,
+                        ),
+                    ),
+                ),
+            ),
+            Positioned(
+                left: 989,
+                top: 700,
+                child: SizedBox(
+                    width: 465,
+                    height: 54,
+                    child: Text(
+                        '© 2024 dot. , Made in the New Administrative Capital with ♡',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15,
+                            fontFamily: 'Hind Kochi',
+                            fontWeight: FontWeight.w500,
+                            height: 0,
+                        ),
+                    ),
+                ),
+            ),
+        ],
+    ),
+)
+    );
+  }
+}
 
 class ProductHomePage extends StatelessWidget {
   @override
@@ -540,31 +776,6 @@ class ProductHomePage extends StatelessWidget {
   }
 }
 
-// ...
-
-class BigCard extends StatelessWidget {
-  const BigCard({
-    super.key,
-    required this.pair,
-  });
-
-  final WordPair pair;
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final style = theme.textTheme.displayLarge!.copyWith(
-      color: theme.colorScheme.onPrimary,
-    );
-    return Card(
-      color: theme.colorScheme.primary,
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Text(pair.asUpperCase, style: style),
-      ),
-    );
-  }
-}
 
 class HistoryPage extends StatelessWidget{
   @override
