0f03181fdc65eb3aae8edad05fce8a5ce5230e6d
Omar Elfiki <oe2300062@tkh.edu.eg>
2024-04-12 16:35:49 +0200

    adding ability to add users internally

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,6 +1,7 @@
 import '../services/navi_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -33,4 +34,61 @@ class AuthService {
   Future<void> signOut() async {
     await FirebaseAuth.instance.signOut();
     }
+
+  Future<String> AddUser(String username, String pass, BuildContext context) async {
+    try {
+      final UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+        email: username,
+        password: pass,
+      );
+      final user = userCredential.user;
+      if (user != null) {
+        final firestore = FirebaseFirestore.instance;
+        final QuerySnapshot querySnapshot = await firestore.collection('User').orderBy(FieldPath.documentId).limitToLast(1).get();
+        final lastDoc = querySnapshot.docs.first;
+        final int nextDocId = int.parse(lastDoc.id) + 1;
+        await firestore.collection('User').doc(nextDocId.toString()).set({
+          'Email': username,
+        });
+        return "User Added";
+      }
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print(e.code);
+        return 'The password provided is too weak.';
+      } else if (e.code == 'email-already-in-use') {
+        print(e.code);
+        return 'The account already exists for that email.';
+      }
+    } catch (e) {
+      print(e);
+      return 'An error occurred. Please try again.';
+    }
+    return 'An error occurred. Please try again.';
   }
+}
+
+class User {
+  final String uid;
+  final String email;
+  // Add more fields as needed
+
+  User({
+    required this.uid,
+    required this.email,
+    // Add more fields as needed
+  });
+}
+
+Future<List<User>> getUsersFromFirestore() async {
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+  final QuerySnapshot querySnapshot = await firestore.collection('User').get();
+  
+  return querySnapshot.docs.map((doc) {
+    return User(
+      uid: doc.id,
+      email: doc['Email'], // Replace with your field name
+      // Add more fields as needed
+    );
+  }).toList();
+}
--- lib/ui/UI_settings/users_spage.dart
+++ lib/ui/UI_settings/users_spage.dart
@@ -1,12 +1,184 @@
+import 'package:dotpos/ui/text_styles.dart';
 import 'package:flutter/material.dart';
+import '../../services/auth_service.dart';
 
 class Users extends StatelessWidget {
+  final Future<List<User>> usersFuture = getUsersFromFirestore();
+  final AuthService authService = AuthService();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Users 2', style: TextStyle(color: Colors.black)),
-      ),
+    return Stack(
+      children: [
+        Expanded(
+          child: AppBar(
+            title: Text('Users', style: settingsHeader),
+          ),
+        ),
+        Positioned(
+          top: 50,
+          left: 0,
+          child: FutureBuilder<List<User>>(
+            future: usersFuture,
+            builder:
+                (BuildContext context, AsyncSnapshot<List<User>> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator();
+              } else if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              } else {
+                return DataTable(
+                  columns: const <DataColumn>[
+                    DataColumn(
+                      label: Text('User ID'),
+                    ),
+                    DataColumn(
+                      label: Text('Email'),
+                    ),
+                    // Add more columns as needed
+                  ],
+                  rows: snapshot.data!.map<DataRow>((User user) {
+                    return DataRow(
+                      cells: <DataCell>[
+                        DataCell(Text(user.uid)),
+                        DataCell(Text(user.email)),
+                        // Add more cells as needed
+                      ],
+                    );
+                  }).toList(),
+                );
+              }
+            },
+          ),
+        ),
+        Positioned(
+            top: 70,
+            left: 500,
+            child: SizedBox(
+              width: 125,
+              height: 40,
+              child: ElevatedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      String username = '';
+                      String password = '';
+                      return AlertDialog(
+                        title: Text('Add User'),
+                        content: Column(
+                          children: <Widget>[
+                            TextFormField(
+                              decoration: InputDecoration(labelText: 'Username'),
+                              onChanged: (value) {
+                                username = value;
+                              },
+                            ),
+                            TextFormField(
+                              decoration: InputDecoration(labelText: 'Password'),
+                              obscureText: true,
+                              onChanged: (value) {
+                                password = value;
+                              },
+                            ),
+                          ],
+                        ),
+                        actions: <Widget>[
+                          TextButton(
+                            child: Text('Cancel'),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                          TextButton(
+                            child: Text('Add'),
+                            onPressed: () async {
+                              String result = await authService.AddUser(
+                                  username, password, context);
+                              showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    if (result == 'User Added') {
+                                      return AlertDialog(
+                                        title: Text('Sucess'),
+                                        icon: Icon(
+                                          Icons.check_circle,
+                                          color: Colors.green,
+                                          size: 30,
+                                        ),
+                                        content: Text(result),
+                                        actions: <Widget>[
+                                          TextButton(
+                                            onPressed: () {
+                                              Navigator.of(context).pop();
+                                            },
+                                            child: Text(
+                                              'OK',
+                                              style:
+                                                  TextStyle(color: Colors.black),
+                                            ),
+                                          ),
+                                        ],
+                                      );
+                                    } else {
+                                      return AlertDialog(
+                                        title: Text('Error'),
+                                        icon: Icon(
+                                          Icons.error,
+                                          color: Colors.red,
+                                          size: 30,
+                                        ),
+                                        content: Text(result),
+                                        actions: <Widget>[
+                                          TextButton(
+                                            onPressed: () {
+                                              Navigator.of(context).pop();
+                                            },
+                                            child: Text(
+                                              'OK',
+                                              style:
+                                                  TextStyle(color: Colors.black),
+                                            ),
+                                          ),
+                                        ],
+                                      );
+                                    }
+                                  });
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                child: Text('Add User', style: TextStyle(color: Colors.black)),
+              ),
+            )),
+        Positioned(
+          left: 500,
+          top: 120,
+          child: SizedBox(
+            width: 125,
+            height: 40,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Text('Delete User', style: TextStyle(color: Colors.red)),
+                  ),
+          )
+        ),
+        Positioned(
+          left: 500,
+          top: 170,
+          child: SizedBox(
+            width: 125,
+            height: 40,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Text('Reset User', style: TextStyle(color: Colors.black)),
+                  ),
+          )
+        ),
+      ],
     );
   }
 }
