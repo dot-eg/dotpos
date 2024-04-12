d2007c52368ca7865db6d204b0bb6ec47d27de99
Omar Elfiki <oe2300062@tkh.edu.eg>
2024-04-12 13:35:47 +0200

    adding improvements to history ui + clear all transactions button

--- lib/services/trans_service.dart
+++ lib/services/trans_service.dart
@@ -17,3 +17,33 @@ Future<String> addTransaction(DateTime datetime, int customer_ID, double amount,
     return 'Failed to add transaction';
   }
 }
+
+Future<void> deleteTransaction(String docId) async {
+  try {
+    final transactionRef = firestore_service.db.collection('Transaction').doc(docId);
+    await transactionRef.delete();
+  } catch (e) {
+    print(e);
+  }
+}
+
+Future<void> updateTransaction(String docId, Map<String, dynamic> data) async {
+  try {
+    final transactionRef = firestore_service.db.collection('Transaction').doc(docId);
+    await transactionRef.update(data);
+  } catch (e) {
+    print(e);
+  }
+}
+
+Future<void> clearAllTransactions() async {
+  try {
+    final transactionRef = firestore_service.db.collection('Transaction');
+    QuerySnapshot querySnapshot = await transactionRef.get();
+    for (QueryDocumentSnapshot doc in querySnapshot.docs) {
+      await doc.reference.delete();
+    }
+  } catch (e) {
+    print(e);
+  }
+}
--- lib/ui/transaction_history.dart
+++ lib/ui/transaction_history.dart
@@ -1,43 +1,60 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import '../services/firestore_service.dart';
+import 'text_styles.dart';
 
 class HistoryPage extends StatelessWidget {
   final Future<List<Map<String, dynamic>>> transactionsFuture = retrieveAllTransactions();
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<Map<String, dynamic>>>(
-      future: transactionsFuture,
-      builder: (BuildContext context, AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(child: CircularProgressIndicator());
-        } else if (snapshot.hasError) {
-          return Center(child: Text('Error: ${snapshot.error}'));
-        } else {
-          List<Map<String, dynamic>> transactions = snapshot.data!;
-          if (transactions.isEmpty) {
-            return Center(child: Text('No history yet'));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Transaction History', style: settingsHeader),
+      ),
+      body: FutureBuilder<List<Map<String, dynamic>>>(
+        future: transactionsFuture,
+        builder: (BuildContext context, AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
           } else {
-            return SizedBox(
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width * 0.3,
-              child: ListView.builder(
-                itemCount: transactions.length,
-                itemBuilder: (BuildContext context, int index) {
-                  Map<String, dynamic> transaction = transactions[index];
-                  return ListTile(
-                    title: Text('Transaction ${index + 1}', style: TextStyle(fontWeight: FontWeight.bold),),
-                    subtitle: Text('Customer ID: ${transaction['Customer_ID']}\n'
-                                  'Amount: ${transaction['Amount']}\n'
-                                  'Date: ${transaction['Datetime']}\n'
-                                  'Products: ${transaction['Items']}'),
-                  );
-                },
-              ),
-            );
+            List<Map<String, dynamic>> transactions = snapshot.data!;
+            if (transactions.isEmpty) {
+              return Center(child: Text('No history yet'));
+            } else {
+              return SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width * 0.3,
+                child: ListView.separated(
+                  itemCount: transactions.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    Map<String, dynamic> transaction = transactions[index];
+                    Timestamp timestamp = transaction['Datetime'];
+                    DateTime datetime = timestamp.toDate();
+                    String formattedDate = DateFormat('dd-MM-yyyy – h:mm a').format(datetime);
+                    return InkWell(
+                      onTap: () {
+                        // Add your onTap logic here
+                      },
+                      child: ListTile(
+                        title: Text('Transaction ${index + 1}', style: TextStyle(fontWeight: FontWeight.bold)),
+                        subtitle: Text('Customer ID: ${transaction['Customer_ID']}\n'
+                                      'Amount: ${transaction['Amount']}\n'
+                                      'Date: $formattedDate\n'
+                                      'Products: ${transaction['Items']}'),
+                      ),
+                    );
+                  },
+                  separatorBuilder: (BuildContext context, int index) => Divider(),
+                ),
+              );
+            }
           }
-        }
-      },
+        },
+      ),
     );
   }
 }
