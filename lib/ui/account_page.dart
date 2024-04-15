ccf7fa7da81366ef39da32f7fcc659286aafbd7a
assal655 <MA2201152@tkh.edu.eg>
2024-04-15 14:20:07 +0200

    edit account page

--- lib/ui/account_page.dart
+++ lib/ui/account_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import '../services/auth_service.dart';
 import '../ui/text_styles.dart';
@@ -5,7 +7,6 @@ import '../ui/text_styles.dart';
 class AccountPage extends StatefulWidget {
   @override
   State<AccountPage> createState() => _AccountPageState();
-  
 }
 
 class _AccountPageState extends State<AccountPage> {
@@ -21,40 +22,78 @@ class _AccountPageState extends State<AccountPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Account', style: TextStyle(fontSize: 30)),
-      ),
-      body: Stack(
-        children:[ 
-          Positioned(
-            top: MediaQuery.of(context).size.height * 0.00630676,
-            left: MediaQuery.of(context).size.width * 0.00976562 ,
-            child: SizedBox(
-              width: MediaQuery.of(context).size.width * 0.21809896,
-              height: MediaQuery.of(context).size.height * 0.03784057,
-              child: Text('Hello!, $currentUser', style: settingsHeader.copyWith(fontWeight: FontWeight.normal),),
+        appBar: AppBar(
+          title: Text('Account', style: TextStyle(fontSize: 30)),
+        ),
+        body: Stack(
+          children: [
+            Positioned(
+              top: MediaQuery.of(context).size.height * 0.00630676,
+              left: MediaQuery.of(context).size.width * 0.00976562,
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.21809896,
+                height: MediaQuery.of(context).size.height * 0.03784057,
+                child: Text(
+                  'Hello!, $currentUser',
+                  style: settingsHeader.copyWith(fontWeight: FontWeight.normal),
+                ),
+              ),
             ),
+          
+            //   Column(
+            //   mainAxisAlignment: MainAxisAlignment.center,
+            //   children: <Widget>[
+            //     Text('My Account Details'),
+            //     Text('Current user: $currentUser'),
+            //     Text('Last login: $loginTime'),
+            
+            Positioned(
+  child: Column(
+              children: [
+                SizedBox(
+                  width: 200, 
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: _onLogout,
+                    child: Text('Logout'),
+                  ),
+                ),
+                SizedBox(
+                  width: 200, 
+                  height: 50, 
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Help'),
+                  ),
+                ),
+                SizedBox(
+                  width: 200, 
+                  height: 50, 
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Notifications'),
+                  ),
+                ),
+                SizedBox(
+                  width: 200, 
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Settings'),
+                  ),
+                ),
+              ],
             ),
-        //   Column(
-        //   mainAxisAlignment: MainAxisAlignment.center,
-        //   children: <Widget>[
-        //     Text('My Account Details'),
-        //     Text('Current user: $currentUser'),
-        //     Text('Last login: $loginTime'),
-        //     Padding(padding: EdgeInsets.all(10)),
-        //     ElevatedButton(
-        //       onPressed: _onLogout,
-        //       child: Text('Logout'),
-        //     ),
-        //     Padding(padding: EdgeInsets.all(10)),
-        //     ElevatedButton(
-        //       onPressed: () {},
-        //       child: Text('Help'),
-        //     ),
-        //   ],
-        // )
-        ],
-    )
+          ),
+            //     Padding(padding: EdgeInsets.all(10)),
+            //     ElevatedButton(
+            //       onPressed: () {},
+            //       child: Text('Help'),
+            //     ),
+            //   ],
+            // )
+          ],
+        ),
     );
   }
 }
